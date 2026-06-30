a8b9bec8c6efa8fdd7b9a67a8339ff6c10ddd0bc
vivek giri <vivekgiri948894@gmail.com>
2026-06-30 09:30:25 +0530

    added 01_basics.js file

---
+++ 01_basics.js
@@ -0,0 +1,20 @@
+/* First java code*/
+
+console.log("Hello World");
+
+// comments = 1-single line comment //
+
+console.log("vivek");
+
+
+/*Thisis the 
+multiple line comment
+*/
+
+let a = "ayush"
+let b = "akash"
+console.log(a);
+console.log(b);
+
+  
+
