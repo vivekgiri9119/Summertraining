cec0257b5992bbc362d59b855e925db502fa2817
vivek giri <vivekgiri948894@gmail.com>
2026-06-30 23:19:25 +0530

    added 04_operators.js file

---
+++ 04_operators.js
@@ -0,0 +1,159 @@
+/*  1.. arithmatice operators */
+
+        //addition//
+
+        let a = 3;
+        let b = 5;
+        console.log(a+b);
+
+        //subtraction//
+        let c = 9;
+        let d = 5;
+        console.log(c-d);
+
+        //multiplication//
+        let e = 9;
+        let f = 5;
+        console.log(e*f);
+
+        //division//
+        let g = 9;
+        let h = 5;
+        console.log(g/h);
+
+        //module// 
+        let i = 4;
+        let j = 5;
+        console.log(i%j);
+
+        //expoential//
+        let k = 2;
+        let l = 3;
+        console.log(k**l);
+
+        //increment//
+        let v = 2;
+        let u = 3;
+        v++ ;
+        console.log(v);
+
+        //pre increment//
+        let vi= 2;
+        let ui= 3;
+        ++vi ;
+        console.log(vi);
+
+        //post increment//
+        let vh= 2;
+        console.log(vh++);
+        console.log(vh);
+
+        //decrement//
+        let C = 2;
+        C--;
+        console.log(C);
+
+        // PRE decrement//
+        let D = 5;
+        console.log(--D);
+        console.log(D);
+        
+
+        //POST decrement//
+        let E = 8;
+        console.log(E--); //8
+        console.log(E);  //7
+        
+
+/* 2..comparison operators //
+
+        /*equalto*/
+        let m = 2;
+        let n = 3;
+        console.log(m==n)
+
+        /*equalto*/
+        let mm = 2;
+        let nn = 3;
+        console.log(mm===nn)
+
+        //not equelto//
+        let w = 2;
+        let x = 3;
+        console.log(w!=x)
+
+        //greater then//
+        let o = 2;
+        let p = 3;
+        console.log(o>p)
+
+        //lessthen//
+        let q = 2;
+        let r = 3;
+        console.log(q<r)
+
+        //greaterthen or equal//
+        let s = 2;
+        let t = 3;
+        console.log(s>=t)
+
+        //lessthen or equal//
+        let aa = 2;
+        let bb = 3;
+        console.log(aa<=bb)
+
+
+/* 3.. assignment operators  */      
+
+        let y = 10;
+        
+        y += 5;
+        console.log("+= :", y);
+
+        y -= 2;
+        console.log("-= :", y);
+
+        y *= 2;
+        console.log("*= :", y);
+
+        y /= 2;
+        console.log("/= :", y);
+
+        y %= 3;
+        console.log("%= :", y);
+
+/* 4.. Logical Operators  */
+        
+       // AND //
+
+       let ff = 24;
+       let gg = 23
+       console.log(ff>26 && gg>28);
+       console.log(ff>20 && gg>20);
+
+       // OR //
+
+       let FF = 24;
+       let GG = 23
+       console.log(FF>26 || GG>28);
+       console.log(FF>20 || GG>20);
+
+       // NOT //
+
+       let hh = 24;
+       let ii = 23
+       console.log(!(hh>26));
+       console.log(!(hh>20));
+
+
+/* 5.. Ternary Operator  */
+
+     let age = 18;
+
+     console.log(age >= 18 ? "Adult" : "Minor");
+     console.log(age >= 20 ? "Adult" : "Minor");
+
+
+
+
+
