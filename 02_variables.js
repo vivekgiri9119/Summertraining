fd5f4b595017a6005a289a73babc8550eb677b1c
vivek giri <vivekgiri948894@gmail.com>
2026-06-30 10:30:01 +0530

    added 02_variable.js file

---
+++ 02_variables.js
@@ -0,0 +1,30 @@
+//variable declaration//
+let name;
+
+//variable initialization//
+name = "vivek";
+
+//declaration+initialization//
+let fullname = "vivek";
+
+//variable Var//
+ var age = 20;
+ console.log(age);
+
+ //variable let//
+ let college = "BIET college";
+ console.log(college);
+
+ // variable const//
+ const pi = 3.99;
+ console.log(pi);
+
+ //variable update//
+ let schoole = "BIET college";
+ schoole = "RIMT college";
+ console.log(schoole);
+
+ //.const ko update nhi kar sakte h isme //
+
+ 
+ 
